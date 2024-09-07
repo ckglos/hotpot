42c4b9ad65462ad32f977fb42c5759a92328ebb1
Dave Cross <davidccross@gmail.com>
2024-09-07 08:34:47 +0100

    Add the CKGlos Dokuwiki template
    Copy of the latest CKGlos Dokuwiki template which makes Dokuwiki into
    HotPot

---
+++ lib/tpl/ckglos/tpl_footer.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template footer, included in the main and detail files
+ */
+
+// must be run from within DokuWiki
+if (!defined('DOKU_INC')) die();
+?>
+
+<!-- ********** FOOTER ********** -->
+<footer id="dokuwiki__footer"><div class="pad">
+
+    <div class="buttons">
+        <p>(C) Community Kitchen Glos 2024, charity 1207428</p>
+    </div>
+
+    <?php tpl_includeFile('footer.html'); ?>
+</div></footer><!-- /footer -->
